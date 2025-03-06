b9281b36da1e6dc52e37235da5d04378e4696229
luco-hub <nan2boom93@gmail.com>
2025-03-05 22:25:55 -0500

    creacion del home

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -6,20 +6,20 @@ class MenuItems {
   final String title;
   final String subTitle;
   final String link;
-  final IconData;
+  final IconData icono;
 
   const MenuItems(
     {
     required this.title, 
     required this.subTitle, 
     required this.link, 
-    required this.IconData});
+    required this.icono});
 
 
 }
 
 const appMenuItems = <MenuItems>[
-MenuItems(title: 'Botones',subTitle: 'varios botones flutter',link: '/buttons',IconData: Icons.smart_button_outlined),
-MenuItems(title: 'Tarjetas',subTitle: 'un contenedor estilizado',link: '/Card',IconData: Icons.credit_card)
+MenuItems(title: 'Botones',subTitle: 'varios botones flutter',link: '/buttons',icono: Icons.smart_button_outlined),
+MenuItems(title: 'Tarjetas',subTitle: 'un contenedor estilizado',link: '/Card',icono: Icons.credit_card)
 
 ];
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
+
+import '../../../config/menu/menu_items.dart';
 
 class HomeScreen  extends StatelessWidget {
   const HomeScreen ({super.key});
@@ -7,9 +10,39 @@ class HomeScreen  extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter / Widges...'),
+        title: const Text('Flutter / Widges...'),
       ),
 
+      body: const _HomeBody(menuItems:appMenuItems,),
+
     );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+
+  final List<MenuItems> menuItems;
+
+  const _HomeBody({super.key, required this.menuItems});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorTheme = Theme.of(context).colorScheme;
+    
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: menuItems.length,
+      itemBuilder:(context, index) => ListTile( 
+          title: Text(menuItems[index].title,),
+          subtitle:Text(menuItems[index].subTitle),
+          leading: Icon( menuItems[index].icono, color:colorTheme.primary,),
+          trailing: Icon(Icons.arrow_forward_ios_rounded,color: colorTheme.primary,),
+          onTap: () {
+            
+          },
+
+      ),
+      
+      );
   }
 }
