ce886f87d5edaf672ab50deed30520505b3567c2
luco-hub <nan2boom93@gmail.com>
2025-03-10 22:14:46 -0500

    tambien

---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SnackBarScreen extends StatelessWidget {
+
+
+  static const String nameRoute = 'snack_screen';
+
+  const SnackBarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+    }
+    
+  
+  }
