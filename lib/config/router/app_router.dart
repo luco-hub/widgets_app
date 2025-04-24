e6c35a8d505f20dd7794c6ba33cda899d3794820
luco-hub <nan2boom93@gmail.com>
2025-04-23 23:46:45 -0500

    animate container

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -8,7 +8,9 @@ final appRouter = GoRouter(
         GoRoute(path: '/',name: HomeScreen.nameRoute,builder: (context, state) => const HomeScreen(),), 
         GoRoute(path: '/buttons',name:  ButtonsScreens.nameRoute,builder: (context, state) => const ButtonsScreens(),), 
         GoRoute(path: '/Card',name: CardsScreens.nameRoute ,builder: (context, state) => const CardsScreens(),) ,
-        GoRoute(path: '/progres',name: ProgressScreen.nameRoute ,builder: (context, state) => const ProgressScreen(),) 
+        GoRoute(path: '/progres',name: ProgressScreen.nameRoute ,builder: (context, state) => const ProgressScreen(),) ,
+        GoRoute(path: '/snack',name: SnackBarScreen.nameRoute ,builder: (context, state) => const SnackBarScreen(),) ,
+        GoRoute(path: '/animations',name: AnimatedScreen.nameRoute ,builder: (context, state) => const AnimatedScreen(),) 
         
         ]
 );
--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,6 +1,7 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
 
 
   static const String nameRoute = 'animate_screen';
@@ -8,7 +9,60 @@ class AnimatedScreen extends StatelessWidget {
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+  double width = 50;
+  double height = 50;
+  Color color = Colors.orange;
+  double border = 10.0;
+
+  void changeShape(){
+
+    final random = Random();
+    width = random.nextInt(300)+50;
+    height = random.nextInt(800)+50;
+  
+    border = random.nextInt(100) + 5.0;
+
+    color =Color.fromRGBO(
+      random.nextInt(28),
+      random.nextInt(255),
+      random.nextInt(255),
+      1
+    );
+    setState(() {});
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+        
+      ),
+
+      body: Center(
+
+        child: AnimatedContainer(
+          duration: const Duration( milliseconds: 400),
+          curve: Curves.easeInOutCubic,
+          height: height,
+          width: width ,
+          decoration: BoxDecoration(
+            color: color,  
+            borderRadius: BorderRadius.circular(border)
+          ),
+
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: const Icon(Icons.play_arrow)),
+    );
   }
 }
